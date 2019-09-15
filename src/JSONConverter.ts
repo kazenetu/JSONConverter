020094c4c42c58b9bdafd3778675d7b9c5191a24
kazenetu <rakudafactory@gmail.com>
2019-09-15 17:15:32 +0900

    コメント追加

--- src/JSONConverter.ts
+++ src/JSONConverter.ts
@@ -1,3 +1,8 @@
+/**
+ * Simple JSONConverter for TypeScript
+ * Copyright 2019, kazenetu
+ * Released under the MIT License.
+ */
 export class JSONConverter<T>
 {
   public serialize(target: T): string {
